9c6709dd5b843c36cc99aa90675e12d83965e4d2
nellys <nellyflyly3006@gmail.com>
2025-09-25 00:39:23 +0300

    changes in start

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -3,8 +3,9 @@ Frequency-driven keyword extraction starter
 """
 
 # pylint:disable=too-many-locals, unused-argument, unused-variable, invalid-name, duplicate-code
+from math import log
 from json import load
-
+from main import calculate_frequencies, calculate_tf, calculate_tfidf, clean_and_tokenize, get_top_n, remove_stop_words
 
 def main() -> None:
     """
@@ -12,10 +13,17 @@ def main() -> None:
     """
     with open("assets/Дюймовочка.txt", "r", encoding="utf-8") as file:
         target_text = file.read()
+        words=clean_and_tokenize(target_text)
     with open("assets/stop_words.txt", "r", encoding="utf-8") as file:
         stop_words = file.read().split("\n")
+        final_words=remove_stop_words(words,stop_words)
+    d_calc=calculate_frequencies(final_words)
+    tf_dict=calculate_tf(d_calc)
     with open("assets/IDF.json", "r", encoding="utf-8") as file:
         idf = load(file)
+        tfidf_dict=calculate_tfidf(tf_dict, idf)
+    print(get_top_n(tfidf_dict, 10))
+
     with open("assets/corpus_frequencies.json", "r", encoding="utf-8") as file:
         corpus_freqs = load(file)
     result = None
