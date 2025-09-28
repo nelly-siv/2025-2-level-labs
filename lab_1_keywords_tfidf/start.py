079fb29683ca4f0c567e88087460144e0beb3dc8
nellys <nellyflyly3006@gmail.com>
2025-09-28 22:30:08 +0300

    new changes 8

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -21,38 +21,27 @@ def main() -> None:
     """
     with open("assets/Дюймовочка.txt", "r", encoding="utf-8") as file:
         target_text = file.read()
-        words=clean_and_tokenize(target_text)
-
-        if words is None:
-            return
-
     with open("assets/stop_words.txt", "r", encoding="utf-8") as file:
         stop_words = file.read().split("\n")
-        final_words=remove_stop_words(words,stop_words)
-
-        if final_words is None:
-            return
-
-        d_calc=calculate_frequencies(final_words)
-
-        if d_calc is None:
-            return
-        print(get_top_n(d_calc, 10))
-        tf_dict=calculate_tf(d_calc)
-
-        if tf_dict is None:
-            return
-
-
     with open("assets/IDF.json", "r", encoding="utf-8") as file:
         idf = load(file)
-        tfidf_dict=calculate_tfidf(tf_dict, idf)
-
-        if tfidf_dict is None:
-            return
-
-        print(get_top_n(tfidf_dict, 10))
-
+    words=clean_and_tokenize(target_text)
+    if words is None:
+        return
+    final_words=remove_stop_words(words,stop_words)
+    if final_words is None:
+        return
+    d_calc=calculate_frequencies(final_words)
+    if d_calc is None:
+        return
+    print(get_top_n(d_calc, 10))
+    tf_dict=calculate_tf(d_calc)
+    if tf_dict is None:
+        return    
+    tfidf_dict=calculate_tfidf(tf_dict, idf)
+    if tfidf_dict is None:
+        return
+    print(get_top_n(tfidf_dict, 10))
     #with open("assets/corpus_frequencies.json", "r", encoding="utf-8") as file:
     #    corpus_freqs = load(file)
     #result = None
