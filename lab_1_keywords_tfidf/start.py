79e3f741d5e9fcfb60daeaef42f3cc717451941e
nellys <nellyflyly3006@gmail.com>
2025-09-28 03:17:08 +0300

    start without log

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -3,7 +3,6 @@ Frequency-driven keyword extraction starter
 """
 
 # pylint:disable=too-many-locals, unused-argument, unused-variable, invalid-name, duplicate-code
-from math import log
 from json import load
 from lab_1_keywords_tfidf.main import (
     calculate_frequencies, 
