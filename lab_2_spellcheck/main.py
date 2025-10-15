04773e99e40911bc5613f47eece3d9eec9a17000
nellys <nellyflyly3006@gmail.com>
2025-10-15 03:44:29 +0300

    new changes 2

--- lab_2_spellcheck/main.py
+++ lab_2_spellcheck/main.py
@@ -4,6 +4,10 @@ Lab 2.
 
 # pylint:disable=unused-argument
 from typing import Literal
+from lab_1_keywords_tfidf.main import(
+    check_dict,
+    check_list,
+)
 
 
 def build_vocabulary(tokens: list[str]) -> dict[str, float] | None:
